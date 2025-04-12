5deb8f1d33015ee77df917d086d55aad08e70ed6
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2025-04-12 13:20:48 -0300

    chore: add type checker script

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
