9714963ce7a73e638fc5c344b76576efb609a9ff
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2025-05-26 15:32:36 -0300

    feat: add sidebar translation

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,15 +1,18 @@
 import { FunctionComponent } from "react";
-import { Accordion, ActionIcon, Tooltip } from "@mantine/core";
+import { Accordion, ActionIcon, Select, Tooltip } from "@mantine/core";
 import { GiPerspectiveDiceOne, GiPerspectiveDiceSixFacesRandom, GiSwapBag, GiHistogram } from "react-icons/gi";
 import { VscTrash } from "react-icons/vsc";
 
 import { INodeType } from "@/config/types";
 import { useReactFlow } from "@xyflow/react";
+import { useTranslation } from "react-i18next";
+import { BiWorld } from "react-icons/bi";
 
 type IProps = { addNewNode: (type: INodeType) => void };
 
 const Sidebar: FunctionComponent<IProps> = ({ addNewNode }) => {
   const flow = useReactFlow();
+  const { t, i18n } = useTranslation();
 
   function clearBoard() {
     flow.setEdges([]);
@@ -20,75 +23,75 @@ const Sidebar: FunctionComponent<IProps> = ({ addNewNode }) => {
     <aside className="text-[14px] text-slate-900 fixed z-20 left-0 top-0 bg-white h-screen w-48 flex flex-col border-r select-none">
       <Accordion variant="contained" chevronPosition="right" defaultValue={["dice"]} classNames={{ content: "p-0", item: "border-x-0" }} multiple>
         <Accordion.Item value="dice">
-          <Accordion.Control icon={<GiPerspectiveDiceOne className="text-[22px]" />}>Dice</Accordion.Control>
+          <Accordion.Control icon={<GiPerspectiveDiceOne className="text-[22px]" />}>{t("common.dice")}</Accordion.Control>
 
           <Accordion.Panel className="bg-white">
             <ul className="flex flex-col">
               <li className="sidebar-item" onClick={() => addNewNode("diceGenerator")}>
-                Dice generator
+                {t("sidebar.diceGenerator")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("diceExplodeGenerator")}>
-                Dice explode generator
+                {t("sidebar.diceExplodeGenerator")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("diceLogical")}>
-                Logical
+                {t("sidebar.diceLogical")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("diceMath")}>
-                Math
+                {t("sidebar.diceMath")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("diceAbsolute")}>
-                Absolute value
+                {t("sidebar.diceAbsolute")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("dicePool")}>
-                Pool
+                {t("sidebar.dicePool")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("dicePoolSum")}>
-                Pool sum
+                {t("sidebar.dicePoolSum")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("diceSuccess")}>
-                Success
+                {t("sidebar.diceSuccess")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("diceBetweenInterval")}>
-                Between interval
+                {t("sidebar.diceBetweenInterval")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("diceCountRepetition")}>
-                Count repetition
+                {t("sidebar.diceCountRepetition")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("valueIsEven")}>
-                Is Even
+                {t("sidebar.valueIsEven")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("valueIsOdd")}>
-                Is Odd
+                {t("sidebar.valueIsOdd")}
               </li>
             </ul>
           </Accordion.Panel>
         </Accordion.Item>
 
         <Accordion.Item value="bag">
-          <Accordion.Control icon={<GiSwapBag className="text-[22px]" />}>Bag</Accordion.Control>
+          <Accordion.Control icon={<GiSwapBag className="text-[22px]" />}>{t("common.bag")}</Accordion.Control>
 
           <Accordion.Panel className="bg-white">
             <ul className="flex flex-col">
               <li className="sidebar-item" onClick={() => addNewNode("bagGenerator")}>
-                Bag generator
+                {t("sidebar.bagGenerator")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("bagPullWithoutRepetition")}>
-                Pull without repetition
+                {t("sidebar.bagPullWithoutRepetition")}
               </li>
             </ul>
           </Accordion.Panel>
         </Accordion.Item>
 
         <Accordion.Item value="symbolic">
-          <Accordion.Control icon={<GiPerspectiveDiceSixFacesRandom className="text-[22px]" />}>Symbolic</Accordion.Control>
+          <Accordion.Control icon={<GiPerspectiveDiceSixFacesRandom className="text-[22px]" />}>{t("common.symbolic")}</Accordion.Control>
 
           <Accordion.Panel className="bg-white">
             <ul className="flex flex-col">
               <li className="sidebar-item" onClick={() => addNewNode("symbolicGenerator")}>
-                Symbolic generator
+                {t("sidebar.symbolicGenerator")}
               </li>
               <li className="sidebar-item" onClick={() => addNewNode("symbolicPool")}>
-                Pool symbolic
+                {t("sidebar.symbolicPool")}
               </li>
             </ul>
           </Accordion.Panel>
@@ -98,26 +101,37 @@ const Sidebar: FunctionComponent<IProps> = ({ addNewNode }) => {
           <Accordion.Control className="px-4 hover:bg-slate-100">
             <div className="flex gap-4 items-center justify-start">
               <GiHistogram className="text-[22px]" />
-              <span>Output</span>
+              <span>{t("common.output")}</span>
             </div>
           </Accordion.Control>
 
           <Accordion.Panel className="bg-white">
             <ul className="flex flex-col">
               <li className="sidebar-item" onClick={() => addNewNode("histogram")}>
-                Histogram
+                {t("sidebar.histogram")}
               </li>
             </ul>
           </Accordion.Panel>
         </Accordion.Item>
       </Accordion>
 
-      <div className="flex gap-2 items-end h-full pb-2 px-2">
-        <Tooltip label="Clear all board nodes">
+      <div className="flex justify-between gap-4 items-end h-full pb-2 px-2">
+        <Tooltip label={t("sidebar.clearTooltip")}>
           <ActionIcon variant="filled" color="red" onClick={clearBoard}>
             <VscTrash />
           </ActionIcon>
         </Tooltip>
+
+        <Select
+          size="xs"
+          leftSection={<BiWorld className="text-[18px]" />}
+          value={i18n.language}
+          onChange={(value) => value && i18n.changeLanguage(value)}
+          data={[
+            { label: t("sidebar.english"), value: "en" },
+            { label: t("sidebar.portuguese"), value: "pt-BR" },
+          ]}
+        />
       </div>
     </aside>
   );
