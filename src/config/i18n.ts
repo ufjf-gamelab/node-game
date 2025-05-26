4014d2ef6965422c7da876358cf8dc1241eb77fb
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2025-05-26 15:27:12 -0300

    feat: add browser language detector

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,23 +1,23 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import translationEN from "@/locale/translations/en.json";
 import translationPTBR from "@/locale/translations/pt-br.json";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: translationEN,
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    supportedLngs: ["en", "pt-BR"],
+    resources: {
+      en: { translation: translationEN },
+      "pt-BR": { translation: translationPTBR },
     },
-    "pt-BR": {
-      translation: translationPTBR,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
     },
-  },
-  lng: "en",
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+  });
 
 export { i18n };
