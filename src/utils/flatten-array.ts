67b7344cb720147ef30a1ceb3124d667b9bd30d9
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2025-06-04 22:00:44 -0300

    fix: improve flatten array

--- src/utils/flatten-array.ts
+++ src/utils/flatten-array.ts
@@ -1,3 +1,4 @@
-export function flattenArray<T>(array: (T | T[])[]): T[] {
+export function flattenArray<T>(array: Array<T | Array<T>>): Array<T> {
+  if (!Array.isArray(array[0])) return array as T[];
   return array.flat(Infinity) as T[];
 }
