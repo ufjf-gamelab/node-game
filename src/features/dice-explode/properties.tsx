3a04c218725fec835f0ed308959d7f068790f78d
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2025-06-06 14:43:58 -0300

    fix: dice explode translation

--- src/features/dice-explode/properties.tsx
+++ src/features/dice-explode/properties.tsx
@@ -4,8 +4,10 @@ import { useReactFlow } from "@xyflow/react";
 import { NumberInput } from "@mantine/core";
 import { BaseNodeProperties } from "@/components/ui/base-node-properties";
 import { IDiceExplodeNode } from "@/config/types";
+import { useTranslation } from "react-i18next";
 
 export const DiceExplodeProperties: React.FunctionComponent<{ node: IDiceExplodeNode }> = ({ node }) => {
+  const { t } = useTranslation();
   const flow = useReactFlow();
   const [explodeFace, setExplodeFace] = React.useState(node.data.explodeFace);
 
@@ -17,5 +19,10 @@ export const DiceExplodeProperties: React.FunctionComponent<{ node: IDiceExplode
 
   useDebounce(() => flow.updateNodeData(node.id, { ...node.data, explodeFace }), 500, [explodeFace]);
 
-  return <BaseNodeProperties node={node} children={<NumberInput label="Explode face" value={explodeFace} onChange={handleChangeExplodeFace} />} />;
+  return (
+    <BaseNodeProperties
+      node={node}
+      children={<NumberInput label={t("nodeProperties.explodeFace")} value={explodeFace} onChange={handleChangeExplodeFace} />}
+    />
+  );
 };
