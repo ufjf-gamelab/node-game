57d06eafe7c1e1128445f782eb75c39857fdbe92
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2025-07-03 10:40:40 -0300

    feat: improve count repetition to accept any input type

--- src/features/dice-count-repetition/properties.tsx
+++ src/features/dice-count-repetition/properties.tsx
@@ -3,7 +3,7 @@ import { BaseNodeProperties } from "@/components/ui/base-node-properties";
 import { IDiceCountRepetitionNode } from "@/config/types";
 import { useReactFlow } from "@xyflow/react";
 import { useDebounce } from "react-use";
-import { NumberInput } from "@mantine/core";
+import { TextInput } from "@mantine/core";
 import { useTranslation } from "react-i18next";
 
 export const DiceCountRepetitionProperties: React.FunctionComponent<{ node: IDiceCountRepetitionNode }> = ({ node }) => {
@@ -11,15 +11,17 @@ export const DiceCountRepetitionProperties: React.FunctionComponent<{ node: IDic
   const flow = useReactFlow();
   const [face, setFace] = React.useState(node.data.face);
 
-  function handleChangeFace(value: string | number) {
-    const newValue = isNaN(Number(value)) ? 0 : Number(value);
-    setFace(newValue);
-    node.data.face = newValue;
+  function handleChangeFace(value: string) {
+    setFace(value);
+    node.data.face = value;
   }
 
   useDebounce(() => flow.updateNodeData(node.id, { ...node.data, face }), 500, [face]);
 
   return (
-    <BaseNodeProperties node={node} children={<NumberInput label={t("nodeProperties.valueToCount")} value={face} onChange={handleChangeFace} />} />
+    <BaseNodeProperties
+      node={node}
+      children={<TextInput label={t("nodeProperties.valueToCount")} value={face} onChange={(e) => handleChangeFace(e.target.value)} />}
+    />
   );
 };
--- src/features/dice-count-repetition/service.ts
+++ src/features/dice-count-repetition/service.ts
@@ -1,5 +1,6 @@
 import { i18n } from "@/config/i18n";
 import { IDiceCountRepetitionNode, INodeService } from "@/config/types";
+import { flattenArray } from "@/utils/flatten-array";
 
 export const DiceCountRepetitionService: INodeService<IDiceCountRepetitionNode> = {
   new(_flow, { id, position }) {
@@ -10,8 +11,8 @@ export const DiceCountRepetitionService: INodeService<IDiceCountRepetitionNode> 
       data: {
         name: i18n.t("nodeShortName.diceCountRepetition"),
         status: "IDLE",
-        face: 1,
-        inputType: "numeric",
+        face: "1",
+        inputType: "any",
         outputType: "numeric",
       },
     };
@@ -21,18 +22,24 @@ export const DiceCountRepetitionService: INodeService<IDiceCountRepetitionNode> 
     const [source] = inputs;
     if (!source) throw new Error("Source connection state not found!");
 
-    const sourceState = source.state as number[];
+    const sourceState = source.state as string[] | string[][] | number[] | number[][];
     const resultState = countRepetition(sourceState, node.data.face);
     return resultState;
   },
 };
 
-function countRepetition(data: number[], face: number) {
+function countRepetition(data: string[] | string[][] | number[] | number[][], face: string) {
   const result: number[] = [];
 
-  data.forEach((item, i) => {
-    if (item === face) result[i] = 1;
-    else result[i] = 0;
+  flattenArray<string | number>(data).forEach((item, i) => {
+    if (typeof item === "string") {
+      if (item == face) result[i] = 1;
+      else result[i] = 0;
+    } else {
+      const numericFace = Number(face);
+      if (item == numericFace) result[i] = 1;
+      else result[i] = 0;
+    }
   });
 
   return result;
